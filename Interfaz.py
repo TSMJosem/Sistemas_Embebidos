f6ec43ec1f16980cd26d037fbad6fe11c4c56da1
TSMJosem <receptorx89@gmail.com>
2026-03-02 09:21:02 -0600

    Creación de interfaz gráfica (falta agregar archivo .ui)

---
+++ Interfaz.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5 import uic, QtWidgets
+
+qtCreatorFile = "nombrearchivo.ui"  # Nombre del archivo
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+        # area de los signals
+
+    # Area de los Slots
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
